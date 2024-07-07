afeb88dd25be0a8167e6767106c25b4aeac0998a
NullBrunk <anasi10@proton.me>
2024-07-07 15:18:50 +0200

    updated old todo link in header

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
                     <img className="h-8 w-auto" src="/favicon.png" alt="Todo react logo" />
                     <div className="hidden ml-6 sm:block flex space-x-4">
                         <a href="#" className="navbar-links text-white bg-gray-900" aria-current="page">Dashboard</a>
-                        <a href="https://nullbrunktodo.netlify.app/" className="navbar-links navbar-links-hover">Example</a>
+                        <a href="https://todo-react-0.netlify.app/" className="navbar-links navbar-links-hover">Example</a>
                         <a href="https://github.com/NullBrunk/Todo.js" className="navbar-links navbar-links-hover">Github</a>
                     </div>
                 </div>
@@ -15,7 +15,7 @@ export function Header() {
             <div className="sm:hidden">
                 <div className="space-y-1 p-2">
                     <a href="#" className="navbar-links-mobile text-white bg-gray-900" aria-current="page">Dashboard</a>
-                    <a href="https://nullbrunktodo.netlify.app/" className="navbar-links-mobile navbar-links-hover">Example</a>
+                    <a href="https://todo-react-0.netlify.app/" className="navbar-links-mobile navbar-links-hover">Example</a>
                     <a href="https://github.com/NullBrunk/Todo.js" className="navbar-links-mobile navbar-links-hover">Github</a>
                 </div>
             </div>
